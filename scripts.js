e3768a8d5d7d9c7056a5ad0526fedc45549b1120
LahavStudio <traviolin@gmail.com>
2025-07-28 02:36:50 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -6,7 +6,9 @@ function flashScreen() {
   const flash = document.createElement('div');
   flash.className = 'flash-overlay';
   document.body.appendChild(flash);
+  flash.style.opacity = 1;
   setTimeout(() => {
+    flash.style.transition = 'opacity .3s';
     flash.style.opacity = 0;
     setTimeout(() => flash.remove(), 300);
   }, 100);
